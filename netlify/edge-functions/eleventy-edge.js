f53315a0b01182aad992c8253c000a38234424d0
Ondreas <ondevron@gmail.com>
2023-04-03 18:06:17 +0200

    Update indentation

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -1,6 +1,6 @@
 import {
-  EleventyEdge,
-  precompiledAppData,
+    EleventyEdge,
+    precompiledAppData,
 } from "./_generated/eleventy-edge-app.js";
 
 import { createClient } from "./utils/_deps.js";
@@ -8,51 +8,51 @@ import { createClient } from "./utils/_deps.js";
 import { SUPABASE_URL, SUPABASE_KEY } from "./utils/env.js";
 
 export default async (request, context) => {
-  const accessToken = context.cookies.get("_access_token");
-  let isUser = false;
-
-  try {
-    let authError;
-    if (accessToken) {
-      // Check if session belongs to a valid user
-      const supabase = await createClient(SUPABASE_URL, SUPABASE_KEY);
-      const {
-        data: { user },
-      } = await supabase.auth.getUser(accessToken);
-
-      try {
-        if (user.error) {
-          authError = user.error.message;
+    const accessToken = context.cookies.get("_access_token");
+    let isUser = false;
+    
+    try {
+        let authError;
+        if (accessToken) {
+            // Check if session belongs to a valid user
+            const supabase = await createClient(SUPABASE_URL, SUPABASE_KEY);
+            const {
+                data: { user },
+            } = await supabase.auth.getUser(accessToken);
+            
+            try {
+                if (user.error) {
+                    authError = user.error.message;
+                }
+            } catch (e) {
+                authError = e;
+            }
+            
+            if (authError) {
+                console.log(authError);
+            } else {
+                isUser = true;
+            }
         }
-      } catch (e) {
-        authError = e;
-      }
-
-      if (authError) {
-        console.log(authError);
-      } else {
-        isUser = true;
-      }
+    } catch (e) {
+        console.log("ERROR", { e });
     }
-  } catch (e) {
-    console.log("ERROR", { e });
-  }
-
-  try {
-    let edge = new EleventyEdge("edge", {
-      request,
-      context,
-      precompiled: precompiledAppData,
-      cookies: [],
-    });
-
-    edge.config((eleventyConfig) => {
-      eleventyConfig.addGlobalData("isUser", isUser);
-    });
-
-    return await edge.handleResponse();
-  } catch (e) {
-    console.log("ERROR", { e });
-    return context.next(e);
-  }
+    
+    try {
+        let edge = new EleventyEdge("edge", {
+            request,
+            context,
+            precompiled: precompiledAppData,
+            cookies: [],
+        });
+        
+        edge.config((eleventyConfig) => {
+            eleventyConfig.addGlobalData("isUser", isUser);
+        });
+        
+        return await edge.handleResponse();
+    } catch (e) {
+        console.log("ERROR", { e });
+        return context.next(e);
+    }
 };
--- netlify/edge-functions/utils/setCookie.js
+++ netlify/edge-functions/utils/setCookie.js
@@ -1,11 +1,11 @@
 export function setCookie(context, name, value, expiration) {
-  context.cookies.set({
-    name,
-    value,
-    path: "/",
-    httpOnly: true,
-    secure: true,
-    sameSite: "Lax",
-    expires: expiration,
-  });
+    context.cookies.set({
+        name,
+        value,
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "Lax",
+        expires: expiration,
+    });
 }
