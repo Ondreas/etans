3567bc7d342fcb2479d74bc91eeb8a56fcbc7007
Ondreas <ondevron@gmail.com>
2023-04-03 20:36:52 +0200

    Update regarding Tailwind CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,25 @@
 const { EleventyEdgePlugin } = require('@11ty/eleventy');
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addWatchTarget('./src/sass/');
+    // eleventyConfig.addWatchTarget('./src/sass/');
+    eleventyConfig.addWatchTarget(
+        './src/data/**/*.{js,json}',
+        './src/assets/css/**/*.css',
+        './src/assets/js/**/*.js',
+        // './src/views/**/*.njk',
+        // './src/views/**/*.md',
+        // './src/views/**/*.html'
+        './src/data/**/*.{js,json}',
+        './src/_includes/**/*.{njk,html,md}',
+        './src/_layouts/**/*.{njk,html,md}',
+        './src/posts/**/*.{njk,html,md}'
+    );
     
-    eleventyConfig.addPassthroughCopy({ './src/static/': '/' });
+    eleventyConfig.addPassthroughCopy({ 
+        './src/static/': '/',
+        './src/assets/fonts': '/assets/fonts',
+        'node_modules/alpinejs/dist/cdn.min.js': '/assets/js/alpine.js',
+    });
     
     eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
     
@@ -21,5 +37,11 @@ module.exports = function (eleventyConfig) {
             output: 'dist',
             layouts: '_layouts',
         },
+        templateFormats: [
+            'md',
+            'njk',
+            'html',
+        ],
+        passthroughFileCopy: true,
     };
 };
